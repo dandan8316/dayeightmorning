63f5611026bad987e88b1d11a042792b19988158
dandan8316 <1246645388@qq.com>
2019-01-14 10:13:20 +0800

    路由守卫的

--- src/components/main.vue
+++ src/components/main.vue
@@ -7,16 +7,6 @@
   export default {
     // 声明周期函数
     beforeCreate() {
-      // 判断是否有token
-      let token = window.sessionStorage.getItem("token");
-      if (token) {
-        // 登录成功之后
-      } else {
-        // 没有token 没有登录
-        this.$message.error("请先登录");
-        // 去登录页 编程式导航
-        this.$router.push("login");
-      }
     },
     methods: {
       logout() {
